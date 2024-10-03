72f49a4200790208884f6bdba85e3a5c9e5eaf6b
Medel4 <santiagomedelmarcos@gmail.com>
2024-10-03 21:27:52 +0200

    sc

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float velocidad;
+    [SerializeField] float fuerza, fuerzaSalto;
+    Rigidbody rb;
+        
     // Start is called before the first frame update
     void Start()
     {
+
+        rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        transform.position += new Vector3 (h, 0, v) * velocidad * Time.deltaTime;
+        rb.AddForce(new Vector3(h, 0, v) * fuerza, ForceMode.Force);
+            
+        
+        if (Input.GetKeyDown("space"))
+        {
+
+            GetComponent<Rigidbody>().AddForce(0, fuerzaSalto, 0, ForceMode.Impulse);
+
+        }
         
     }
 }
